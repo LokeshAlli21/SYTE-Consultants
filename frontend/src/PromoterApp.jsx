1950496c66315ca4db204ae26fbe7cab3be8bf0b
Lokesh <lokeshalli1807@gmail.com>
2025-06-30 13:30:23 +0530

    create promoter app .jsx

---
+++ frontend/src/PromoterApp.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import authService from './backend-services/auth/auth';
+import { login, logout} from './store/authSlice'
+
+function PromoterApp() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login(userData))
+      } else {
+        dispatch(logout())
+      }
+      console.log("userData : ",userData);
+    })
+    .catch((error) => console.log("Login Error : ",error))
+  }, [])
+
+  return (
+    <div className="flex  max-w-screen w-full  bg-[#F3F4FF]">
+      <ToastContainer position='top-right' />
+      <div className=" flex-1 flex-grow mx-auto">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+
+export default PromoterApp
