6a830c9443e8455d96161c302ffaeea1979837f9
Lokesh <lokeshalli1807@gmail.com>
2025-07-04 18:45:31 +0530

    try debug

--- frontend-for-promoter/src/backend-services/database/database.js
+++ frontend-for-promoter/src/backend-services/database/database.js
@@ -5,8 +5,6 @@ import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 
-import { getNavigate } from '../../utils/navigation';
-
 import {authenticatedFetch} from '../fetchWrapper';
 
 dayjs.extend(utc);
