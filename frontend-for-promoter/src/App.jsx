3e6e1e81ef77cf4a71e27163ebb23bbbd0162b73
Lokesh <lokeshalli1807@gmail.com>
2025-07-04 19:37:59 +0530

    update cp UI

--- frontend-for-promoter/src/App.jsx
+++ frontend-for-promoter/src/App.jsx
@@ -56,10 +56,10 @@ useEffect(() => {
   }, [])
 
   return (
-    <div className="flex flex-col max-w-screen w-full mx-auto bg-gradient-to-br from-slate-100 via-blue-100 to-indigo-100">
+    <div className="flex flex-col max-w-screen w-full mx-auto bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50">
       <ToastContainer position='top-right' />
       <Header />
-      <div className=" flex-1 flex-grow mx-auto max-w-6xl  px-4 py-8 ">
+      <div className=" flex-1 flex-grow mx-auto max-w-6xl  px-4 pb-8 pt-4">
         <Outlet />
       </div>
     </div>
