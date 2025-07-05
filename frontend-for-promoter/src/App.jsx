00c04c98171d4258df6a4d7931a052bbbc03aed2
Lokesh <lokeshalli1807@gmail.com>
2025-07-05 12:15:41 +0530

    update cp UI

--- frontend-for-promoter/src/App.jsx
+++ frontend-for-promoter/src/App.jsx
@@ -58,6 +58,12 @@ useEffect(() => {
   return (
     <div className="flex flex-col max-w-screen w-full mx-auto bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100">
       <ToastContainer position='top-right' />
+            {/* Animated Background Elements */}
+      <div className="absolute inset-0">
+        <div className="absolute top-20 left-10 w-72 h-72 bg-blue-200/30 rounded-full blur-3xl animate-pulse"></div>
+        <div className="absolute bottom-20 right-10 w-96 h-96 bg-indigo-200/30 rounded-full blur-3xl animate-pulse delay-1000"></div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-purple-200/30 rounded-full blur-3xl animate-pulse delay-500"></div>
+      </div>
       <Header />
       <div className=" flex-1 flex-grow mx-auto max-w-6xl  px-4 pb-8 pt-4">
         <Outlet />
