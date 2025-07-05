54fd07d1f7b9b75d696e483cad17cc10c6ebd6f0
Lokesh <lokeshalli1807@gmail.com>
2025-07-05 12:19:52 +0530

    update cp UI

--- frontend-for-promoter/src/pages/Login.jsx
+++ frontend-for-promoter/src/pages/Login.jsx
@@ -88,9 +88,16 @@ const Login = () => {
 
   return (
     <div className="min-h-screen relative overflow-hidden">
+      {/* Animated Background Elements */}
+      <div className="absolute inset-0">
+        <div className="absolute top-20 left-10 w-72 h-72 bg-blue-200/30 rounded-full blur-3xl animate-pulse"></div>
+        <div className="absolute bottom-20 right-10 w-96 h-96 bg-indigo-200/30 rounded-full blur-3xl animate-pulse delay-1000"></div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-purple-200/30 rounded-full blur-3xl animate-pulse delay-500"></div>
+      </div>
 
       {/* Content */}
       <div className="relative z-10 min-h-screen flex items-center justify-center">
+        <div className="w-full">
 
 
           {/* Login Card */}
@@ -226,6 +233,7 @@ const Login = () => {
               </div>
             </div>
           </div>
+        </div>
       </div>
 
       {/* Custom Styles */}
