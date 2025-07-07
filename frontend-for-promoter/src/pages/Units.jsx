486372ae5ad97ba6c85027e1d3aa88901d0899d7
Lokesh <lokeshalli1807@gmail.com>
2025-07-07 13:42:41 +0530

    debug

--- frontend-for-promoter/src/pages/Units.jsx
+++ frontend-for-promoter/src/pages/Units.jsx
@@ -125,7 +125,7 @@ function Units() {
   }
 
   return (
-    <div className="space-y-6">
+    <div className="space-y-6 max-w-[95%] mx-auto ">
       {/* Header */}
       <div className="bg-white rounded-2xl shadow-sm border border-gray-100 p-6">
         <div className="flex items-center justify-between mb-4">
