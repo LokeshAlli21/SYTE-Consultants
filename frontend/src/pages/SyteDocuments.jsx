89cc8b8aed598df1230d8aed94c551f16d82c4ea
Lokesh <lokeshalli1807@gmail.com>
2025-07-08 17:55:59 +0530

    debug

--- frontend/src/pages/SyteDocuments.jsx
+++ frontend/src/pages/SyteDocuments.jsx
@@ -576,7 +576,7 @@ const SyteDocuments = () => {
             )}
 
             {/* Grid View */}
-            {!isLoading && viewMode === 'grid' && filteredItems.length > 0 && (
+            {!isLoading && viewMode === 'grid' && filteredItems.length > 0 && (currentFolder !== '') &&(
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
                 {filteredItems.map((item) => (
                   <div
